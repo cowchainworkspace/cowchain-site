6cfcbfa549cca48c291b38abfd0c052e5254ccf7
Dmytro Pivkopa <alien_dima@yahoo.com>
2024-08-09 13:27:53 +0300

    fix_build_issues

--- src/app/(dashboard)/team/page.jsx
+++ src/app/(dashboard)/team/page.jsx
@@ -16,7 +16,7 @@ const photos = [team1, team2, team3];
 
 export const metadata = {
   title: "Our Team | Cowchain",
-  description: "Meet the blockchain trend-setters behind Cowchain. Find our group of master engineers and specialists committed to conveying cutting-edge arrangements for your trade challenges.",
+  description: 'Meet the blockchain trend-setters behind Cowchain. Find our group of master engineers and specialists committed to conveying cutting-edge arrangements for your trade challenges.',
   metadataBase: new URL("https://cowchain.io/"),
   alternates: {
     canonical: "/team"
