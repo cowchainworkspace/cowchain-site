ee5f6db327d7984cf73249b460c4f0e38a1025a8
Dmytro Pivkopa <dmytro.pivkopa@cowchain.io>
2024-08-20 22:00:24 +0300

    fix/c20-145/fix_photo_block

--- src/app/(dashboard)/team/blocks/team-info.jsx
+++ src/app/(dashboard)/team/blocks/team-info.jsx
@@ -18,19 +18,31 @@ export default function TeamInfo() {
   return (
     <motion.div
       transition={{ from: 20, duration: 1 }}
-      className="mt-10 border-b border-b-th-fade md:pb-[124px] md:mt-24  xl:mt-28"
+      className="mt-10 border-b px-[20px] border-b-th-fade xl:pb-[124px] md:mt-24  xl:mt-28"
     >
-      <h3 className=" mx-5 mb-20 ml-5 max-w-[740px] text-2xl xl:mb-0 xl:ml-28  xl:text-4xl">
+      <h3 className="text-left hidden sm:block sm:text-center mb-20 max-w-[740px] m-auto xl:max-w-[1174px] text-2xl xl:mb-0  xl:text-4xl">
         SPLIT ACROSS{" "}
         <Image
           src={ua}
           className="mx-2 mb-2 inline-block max-h-[25px] max-w-[25px]"
           alt="ua"
         />
-        UKRAINE AND EUROPE, WE WORK HARD AT FOSTERING AN ENVIRONMENT OF
+        UKRAINE AND EUROPE, WE WORK HARD AT FOSTERING AN  ENVIRONMENT OF 
         TOGETHERNESS, CREATIVITY, AND CLOSE COLLABORATION.
       </h3>
-      <div className="flex flex-col items-center">
+
+      <h3 className="text-left block sm:hidden md:text-center mb-20 max-w-[740px] m-auto xl:max-w-[1174px] text-2xl xl:mb-0  xl:text-4xl">
+        SPLIT ACROSS{" "}
+        <Image
+          src={ua}
+          className="mx-2 mb-2 inline-block max-h-[25px] max-w-[25px]"
+          alt="ua"
+        />
+        <br />
+        UKRAINE AND EUROPE, <br /> WE WORK HARD AT <br />  FOSTERING AN <br /> ENVIRONMENT OF <br />
+        TOGETHERNESS, <br /> CREATIVITY, AND CLOSE <br /> COLLABORATION.
+      </h3>
+      {/* <div className="flex flex-col items-center">
         <h1 className="mt-16 hidden text-center text-9xl xl:block">
           team photo
         </h1>
@@ -38,7 +50,7 @@ export default function TeamInfo() {
           <Image className="hidden md:block w-full max-w-[915px]" src={team} alt="" />
           <Image className="block md:hidden w-full max-w-[915px]" src={teamMobile} alt="" />
         </motion.div>
-      </div>
+      </div> */}
     </motion.div>
   );
 }
