2526ed91dcf8712ce08e2417d55558f6e4b93f52
Dmytro Pivkopa <dmytro.pivkopa@cowchain.io>
2024-12-10 12:56:14 +0200

    fix_build_issue

--- src/app/(dashboard)/services/page.jsx
+++ src/app/(dashboard)/services/page.jsx
@@ -6,15 +6,6 @@ import { Products } from "./blocks/Products";
 import { ViewMoreSection } from "./blocks/ViewMore";
 import Stack from "@/components/stack";
 
-export const metadata = {
-  title: "Our Services | Cowchain",
-  description: "Explore Cowchain's blockchain services, from custom advancement to execution, driving development over industries.",
-  metadataBase: new URL("https://cowchain.io/"),
-  alternates: {
-    canonical: "/services"
-  }
-};
-
 const Services = ({ setBurgerOpen }) => {
   return (
     <section>
