107991171fb8239bdc2af4b7bbb0d5f87487cf39
Viktor Shaparenko <vkshadyafk@gmail.com>
2025-03-03 15:48:08 +0200

    fix/C20-194/defi-development(fix pictures ratio)

--- src/app/(dashboard)/blog/articles/components/Post.jsx
+++ src/app/(dashboard)/blog/articles/components/Post.jsx
@@ -2,19 +2,20 @@ import Image from "next/image";
 
 export const Post = ({ attributes }) => {
   return (
-    <article className="relative w-[320px] border-r border-r-th-fade lg:w-[368px]">
-      <Image
-        height={360}
-        width={672}
-        className="max-h-[233px] w-full self-center object-cover object-fill group-hover:scale-95 md:aspect-square  md:h-auto md:max-h-[370px]"
-        src={attributes?.preview_article_img?.data?.attributes?.url}
-        alt="article image"
+    <article className="relative  h-[479px] w-[271px] overflow-hidden border-r border-r-th-fade md:h-[622px] lg:w-[368px]">
+      <div
+        className="h-[233px] w-[271px] group-hover:scale-95 md:h-[370px] md:w-[368px]"
+        style={{
+          backgroundImage: `url(${attributes?.preview_article_img?.data?.attributes?.url})`,
+          backgroundPosition: "left",
+          backgroundSize: "cover"
+        }}
       />
       <div className="p-5 md:p-6">
-        <h2 className="mb-4 text-pretty md:mb-6 lg:!text-lg">
+        <h2 className="mb-4 line-clamp-3 text-pretty md:mb-6 md:line-clamp-2 lg:!text-lg">
           {attributes?.article_title}
         </h2>
-        <div className="mb-[34px] line-clamp-3 text-balance !text-[#bbb] md:mb-14">
+        <div className="mb-[34px] line-clamp-4 text-balance !text-[#bbb] md:mb-14 md:line-clamp-3">
           {attributes?.article_description?.map((text) => (
             <p className="text-pretty text-sm text-secondary">
               {text.children[0]?.text}
