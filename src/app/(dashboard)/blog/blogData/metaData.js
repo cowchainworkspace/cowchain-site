dd4a80fc0c28ab0708cc2cc11a1ee164e561394c
Viktor Shaparenko <vkshadyafk@gmail.com>
2025-03-12 13:07:26 +0200

    fix/add-description-metadata

--- src/app/(dashboard)/blog/blogData/metaData.js
+++ src/app/(dashboard)/blog/blogData/metaData.js
@@ -1,6 +1,6 @@
 export const metadata = {
   title: "Blog | Cowchain",
-  description: "Insights & more.",
+  description: "Discover blockchain insights, case studies, and industry trends on the Cowchain Blog. Stay updated on Web3, DeFi, and digital asset innovations.",
   metadataBase: new URL("https://cowchain.io/"),
   alternates: {
     canonical: "/blog"
