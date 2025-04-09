33b47c2a9097831e2f5cfd797f601c48b34e6aa8
Viktor Shaparenko <vkshadyafk@gmail.com>
2025-04-09 17:02:46 +0300

    fix/C20-247-cases(change width WhiteLabelCases component)

--- src/app/(dashboard)/services/white_label_solutions/blocks/WhiteLabelCases.jsx
+++ src/app/(dashboard)/services/white_label_solutions/blocks/WhiteLabelCases.jsx
@@ -6,13 +6,13 @@ import React from "react";
 export const WhiteLabelCases = () => {
   return (
     <section className="relative">
-      <div className="relative px-4 py-10 md:px-[88px] md:pb-[84px] md:pt-[126px]">
+      <div className="relative mx-auto max-w-[1440px] px-5 py-10 md:px-8 md:pb-[84px] md:pt-[126px] xl:px-[88px]">
         <div className="flex flex-col items-start justify-between md:flex-row ">
           <div className="flex flex-col gap-5 md:gap-[60px]">
-            <h2 className="lg:order-0 heading-[38px] text-[42px]  uppercase text-white">
+            <h2 className="lg:order-0 heading-[38px] text-[42px]   uppercase text-white">
               Our White Label Web Development Cases
             </h2>
-            <p className="body my-[40px] max-w-lg text-base leading-6 md:my-[20px] lg:order-2 lg:max-w-xl">
+            <p className="body my-[40px]  max-w-lg text-base leading-6 md:my-[20px] lg:order-2 lg:max-w-[637px]">
               Numerous projects, such as Pixeltap, have benefited from our white
               label <br className="hidden lg:block" /> development services,
               enabling them to scale their platforms while maintaining{" "}
@@ -23,7 +23,7 @@ export const WhiteLabelCases = () => {
           <Link href="/cases" className=" lg:order-1 lg:ml-auto lg:block">
             <div className="flex items-center gap-x-2">
               <p className="header uppercase text-white underline">All Cases</p>
-              <Image className="w-6" src={arrow} alt=""></Image>
+              <Image className="w-6" src={arrow} alt="" />
             </div>
           </Link>
         </div>
