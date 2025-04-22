94607a4535cd53a2f71051ba738ee96abcf84b61
Viktor Shaparenko <vkshadyafk@gmail.com>
2025-04-22 10:47:04 +0300

    feat/C20-211-project-gbc-d-app-mobile(fix rewards block positon)

--- src/app/(dashboard)/cases/gbc/components/GBCRewards/index.jsx
+++ src/app/(dashboard)/cases/gbc/components/GBCRewards/index.jsx
@@ -17,13 +17,14 @@ const GBCRewards = () => {
               Unlock new assets and rewards the longer you stay in the Pass.
             </p>
           </div>
-          <div className="grid w-full grid-cols-2 md:grid-cols-3 xl:flex xl:justify-between">
-            <article className="flex w-[177px] flex-col gap-[15px] md:justify-self-center">
+          <div className="grid w-full grid-cols-3 gap-x-1 gap-y-3 md:grid-cols-3 xl:flex xl:justify-between">
+            <article className="flex w-30 flex-col gap-3 justify-self-center md:w-[177px] md:gap-[15px]">
               <Image
                 src={pass.img}
                 width={pass.width}
                 height={pass.height}
                 alt={pass.imgDesc}
+                className="size-[110px] md:size-[177px]"
               />
               <h4 className="align-[text-top] font-roc text-[16.08px] font-bold leading-none text-white">
                 {pass.title}
@@ -35,12 +36,18 @@ const GBCRewards = () => {
             {rewards.map(({ id, imgDesc, width, height, title, img }) => (
               <article
                 className={cn(
-                  "flex w-[166px] flex-col gap-[21px] justify-self-center rounded-[12.87px] border-[1.61px] border-[#4C83FF] p-3",
+                  "flex w-[120px] flex-col items-center gap-3 justify-self-center rounded-[12.87px] border-[1.61px] border-[#4C83FF] p-1 md:w-[166px] md:items-start md:gap-[21px] md:p-3",
                   id > 2 && "border-[#FFFFFF14]"
                 )}
                 key={id}
               >
-                <Image src={img} width={width} height={height} alt={imgDesc} />
+                <Image
+                  src={img}
+                  width={width}
+                  height={height}
+                  alt={imgDesc}
+                  className="size-[100px] md:size-[140px]"
+                />
                 <h4 className="align-[text-top] text-[12.87px] font-bold leading-none text-white">
                   {title}
                 </h4>
