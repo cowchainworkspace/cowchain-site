09938081e917488098625653dda5635c07a1e5ab
pivkopa <121860030+pivkopa@users.noreply.github.com>
2025-04-25 18:57:35 +0300

    Feat/c20 222/staking page (#117)
    * feat/c20-204/nft_game_dev_page
    
    * feat/c20-222/staking_page

---
+++ src/app/(dashboard)/services/staking/utils/data.js
@@ -0,0 +1,273 @@
+import ConsultingAdvisoryIcon from "@/assets/svgComponents/industries/ConsultingAdvisoryIcon";
+import DedicateTeamIcon from "@/assets/svgComponents/industries/DedicateTeamIcon";
+import DesignDevIcon from "@/assets/svgComponents/industries/DesignDevIcon";
+import GovernmentIcon from "@/assets/svgComponents/industries/GovermentIcon";
+import HelthcareIcon from "@/assets/svgComponents/industries/HelthcareIcon";
+import HybridIcon from "@/assets/svgComponents/industries/HybridIcon";
+import LaunchMaintenanceIcon from "@/assets/svgComponents/industries/LaunchMaitenanceIcon";
+import PlanningConsultingIcon from "@/assets/svgComponents/industries/PlanningConsaltingIcon";
+import ProjectBasedIcon from "@/assets/svgComponents/industries/ProjectBasedIcon";
+import SupplyChain from "@/assets/svgComponents/industries/SupplyChainIcon";
+import TestingIcon from "@/assets/svgComponents/industries/TestingIcon";
+import { SeamlessBlockchainIcon } from "@/assets/svgComponents/lottery/SeamlessBlockchainIcon"
+import { EnsuringCrossPlatformIcon } from "@/assets/svgComponents/web3Design/EnsuringCrossPlatformIcon";
+
+import { BankGradeSecurIcon } from '@/assets/svgComponents/staking/BankGradeSecurIcon';
+import { BlockChainIcon } from '@/assets/svgComponents/staking/BlockChainIcon';
+import { FastDevIcon } from '@/assets/svgComponents/staking/FastDevIcon';
+import { WeProvideStakingIcon } from '@/assets/svgComponents/staking/WeProvideStakingIcon';
+import { YouCustomizeIcon } from '@/assets/svgComponents/staking/YouCustomizeIcon';
+
+
+export const DexExpertiseDesc =
+  "We believe that a solid foundation in decentralized exchange development is key to creating platforms that not only meet today’s demands but also evolve with the growing needs of the cryptocurrency market";
+
+export const DexExpertiseData = [
+  {
+    title: "Integration Services and Solutions",
+    desc: "The decentralized exchange development solutions at Cowchain guarantee that third-party integrations, wallets, payment gateways, and liquidity providers will be seamlessly integrated into your system to ensure operations run without glitches and are accessible for users on different platforms. Using advanced decentralized exchange development, we make sure your platform integrates necessary third-party utilities in a non-compromising way regarding performance or security"
+  },
+  {
+    title: "Bespoke Decentralized Exchange App Development",
+    desc: "We offer decentralized exchange development to create unique, user-friendly DEX applications that allow seamless transactions. From crafting smart contracts to building intuitive interfaces, we ensure each platform provides a premium user experience"
+  },
+  {
+    title: "White Label Decentralized Exchange Software",
+    desc: "For companies looking for a quick launch, our decentralized exchange software development services provide a customizable solution that can be branded and scaled to meet your business goals"
+  }
+];
+
+export const IndustriesDexData = [
+  {
+    title: "Government",
+    text: "For government agencies, decentralized technology offers a secure way to manage public services, voting systems, and identity verification. Our blockchain consulting group assists government organizations in implementing solutions that enhance transparency, reduce bureaucracy, and improve citizen services",
+    icon: GovernmentIcon
+  },
+  {
+    title: "Supply Chain Management",
+    text: "Technology enhances supply chain transparency by providing end-to-end traceability of goods and materials. Our blockchain consulting services help businesses in the logistics and supply chain industry implement solutions that improve product tracking, reduce fraud, and streamline communication between suppliers and buyers",
+    icon: SupplyChain
+  },
+  {
+    title: "Healthcare",
+    text: "Security and privacy are crucial in the healthcare sector, where safeguarding sensitive patient information is essential. Blockchain consultants assist healthcare providers in adopting systems that protect data, uphold privacy, and align with regulatory standards, allowing for improved patient care and streamlined operations",
+    icon: HelthcareIcon
+  }
+];
+
+export const DexAppDevData = [
+  {
+    title: "Planning and Consultation",
+    text: "Understanding your project’s requirements",
+    icon: PlanningConsultingIcon
+  },
+  {
+    title: "Design & Development",
+    text: "Government agencies benefit from decentralized platforms for public services such as secure voting systems and identity verification",
+    icon: DesignDevIcon
+  },
+  {
+    title: "Testing",
+    text: "Rigorous testing to ensure system reliability",
+    icon: TestingIcon
+  },
+  {
+    title: "Launch & Maintenance",
+    text: "Continuous maintenance post-launch",
+    icon: LaunchMaintenanceIcon
+  }
+];
+
+export const EngagementDevData = [
+  {
+    title: "Dedicated Team",
+    text: "Ideal for long-term projects, this model offers a team fully committed to your initiative, with our blockchain consultants working closely with you for consistent development and progress",
+    icon: DedicateTeamIcon
+  },
+  {
+    title: "Project-Based",
+    text: "For projects with defined objectives, we manage the entire process, from planning to completion, ensuring timely delivery and clear outcomes through our blockchain consulting",
+    icon: ProjectBasedIcon
+  },
+  {
+    title: "Consulting & Advisory",
+    text: "This option focuses on providing expert guidance, helping businesses make informed decisions on integrating decentralized technologies into their operations",
+    icon: ConsultingAdvisoryIcon
+  },
+  {
+    title: "Hybrid",
+    text: "A blend of ongoing team support and strategic consulting, this model offers adaptability for businesses requiring both hands-on work and high-level advice",
+    icon: HybridIcon
+  }
+];
+
+export const otherServices = [
+  {
+    text: "DApp Development",
+    author:
+      "Our team develops decentralized applications designed to enhance operational capabilities, security, and scalability. These applications are ideal for businesses looking to expand their digital offerings, whether it’s for customer engagement, supply chain transparency, or internal process automation"
+  },
+  {
+    text: "Token Creation and NFT Development",
+    author:
+      "Our blockchain development services include creating custom tokens and NFT development for projects in the blockchain ecosystem. Whether for fundraising, utility, or asset representation, we help you develop the right tokenomics"
+  },
+  {
+    text: "Smart Contract Development",
+    author:
+      "We specialize in developing secure smart contracts, ensuring the integrity of transactions across various platforms. Our contracts are designed with a focus on reliability and transparency, making them essential for industries that rely on precise, automated processes"
+  },
+  {
+    text: "DEX Development",
+    author:
+      "Cowchain develops decentralized exchanges (DEX) designed for fast and secure digital asset trading. Our DEX solutions support token swaps, liquidity pools, and multi-chain compatibility"
+  }
+];
+
+export const otherStakingServices = [
+  {
+    text: "UI / UX Design",
+    author:
+      "We craft intuitive and aesthetically pleasing UI/UX designs, focusing on functionality and user experience to ensure seamless interactions across platforms"
+  },
+  {
+    text: "DISCOVERY & CONSULTATIONS",
+    author:
+      "Our team provides strategic consultations, offering insights on project discovery, business goals, and digital strategies, helping align your web development projects with industry standards"
+  },
+  {
+    text: "Full-Stack Development",
+    author:
+      "From front-end design to back-end systems, our full-stack development team delivers scalable, high-performance web applications that meet your business needs"
+  }
+];
+
+export const web3DesignTrendsData = [
+  {
+    title: "Minimalist Aesthetics in Blockchain Design",
+    content:
+      "Minimalist design enhances user focus by eliminating visual clutter, creating an interface that communicates key actions without overwhelming users. With clean layouts and reduced complexity, minimalist interfaces improve focus and usability, particularly for blockchain interactions that benefit from simplicity."
+  },
+  {
+    title: "Personalization in Blockchain Solutions",
+    content: `Customizable dashboards and adaptive interfaces allow Web3 applications to cater to individual user preferences. Personalized features, like tailored transaction histories or theme adjustments, enhance engagement and usability, keeping users invested in the system.`
+  },
+  {
+    title: "Integration of Advanced Technologies",
+    content: `The incorporation of advanced technologies, such as AI and machine learning, is transforming Web3 interface design. By utilizing predictive analytics and user behavior insights, platforms can anticipate needs and provide tailored recommendations. These innovations enable smarter workflows, like automatically suggesting optimized transaction settings or highlighting security alerts, enhancing both user convenience and confidence.`
+  }
+];
+
+export const stakingFAQData = [
+  {
+    title: "What does white-label web design and development involve?",
+    content:
+      "Such services allow organizations to offer complete web solutions for their clients without maintaining an internal development team. The work is done by a white label development company, and you maintain control of the client relationship and branding"
+  },
+  {
+    title: "How does the white-label development process work?",
+    content: `We begin by gathering detailed project requirements, followed by creating mockups or prototypes for initial feedback. Once finalized, our development team handles the technical implementation. Our process is fully transparent, keeping you informed at every stage`
+  },
+  {
+    title: "What key elements should be included in a white-label web design contract?",
+    content: `It’s important to include clear terms regarding delivery timelines, project scope, and ownership of the final product. As a trusted white label partner, we ensure all legal and technical details are covered`
+  }
+];
+
+export const metadata = {
+  metadataBase: new URL("https://cowchain.io/services"),
+  alternates: {
+    canonical: "/dex_development"
+  }
+};
+
+export const essentialStakingData = [
+  {
+    title: "Blockchain Integration",
+    desc: "A robust staking platform setup requires seamless integration with multiple blockchain networks. White label staking solutions support various consensus mechanisms, including Proof-of-Stake (PoS), Delegated PoS (DPoS), and Liquid Staking Protocols, ensuring wide compatibility across different blockchain ecosystems"
+  },
+  {
+    title: "User-Friendly Interface",
+    desc: "A well-designed, intuitive interface ensures smooth user onboarding and engagement. Custom staking software must include clear staking options, real-time yield tracking, and portfolio management tools to enhance the user experience"
+  }
+];
+
+export const realLifeAppWeb3DesignData = [
+  {
+    title: "Blockchain Lottery Platform Example",
+    desc: "One example of exceptional blockchain UI design is a lottery platform where simplicity is critical. Clear instructions, easy wallet integration, and real-time prize tracking demonstrate how effective Web3 design can ensure reliability and user satisfaction"
+  },
+  {
+    title: "Use Cases in Other Industries",
+    desc: "Web3 design trends are also transforming industries like fintech, gaming, and NFT marketplaces. For example, blockchain UX in NFT trading solutions combines visual appeal with robust functionality. In DeFi, intuitive dashboards simplify lending and staking for both retail investors and institutional participants, creating a bridge between technical complexity and usability"
+  }
+];
+
+export const chooseCryptocurrencyData = [
+  {
+    title: "Evaluating Experience in Exchange Software Development",
+    desc: "Experience matters when choosing a development partner. Our group has a track record of developing dependable, inventive, and growth-ready cryptocurrency exchange solutions. In order to provide you the assurance that your platform is in capable hands, we offer end-to-end development services, supported by client endorsements and a portfolio of projects."
+  },
+  {
+    title: "Assessing Blockchain  and Multi-Currency Support",
+    desc: "Choose a team that excels in blockchain integration and supports a wide range of cryptocurrencies. Our expertise in creating multi-currency exchange platforms establishes your platform will cater to a diverse audience and remain competitive."
+  }
+];
+
+export const advantagesStakingData = [
+  {
+    title: "Time and Cost Savings",
+    desc: "Developing a staking platform from scratch requires significant technical expertise, time, and financial investment. White label solutions eliminate these barriers by providing a pre-built system that reduces development time and lowers costs, allowing businesses to launch a staking service quickly."
+  },
+  {
+    title: "Enhanced Security Features",
+    desc: "Security is critical in staking platform development. White label staking platforms come with built-in security measures, including smart contract validation, node protection, and automated fraud detection. These features ensure that funds remain secure, transactions are protected, and users can stake assets with confidence."
+  },
+  {
+    title: "Scalability and Flexibility",
+    desc: "A staking platform must adapt to growing demand. White label solutions are designed for scalability, allowing businesses to add more assets, expand to new blockchain networks, and integrate custom features without disrupting operations."
+  }
+];
+
+export const paymentSystemData = [
+  {
+    title: "Support for Fiat Gateways",
+    desc: "Enable users to buy cryptocurrencies with traditional payment methods like credit cards or Apple Pay, lowering barriers for new users."
+  },
+  {
+    title: "Instant Cross-Border Payments",
+    desc: "Facilitate real-time, low-cost international transactions by eliminating intermediaries and leveraging blockchain efficiency."
+  },
+  {
+    title: "POS System Integration",
+    desc: "Allow businesses to accept cryptocurrencies in physical stores through seamless Point-of-Sale (POS) system integration."
+  }
+];
+
+export const steByStepStakingData = [
+  {
+    title: "We Provide a Battle-Tested Staking Infrastructure",
+    text: "Forget about expensive development cycles. Our staking software solutions are fully built, audited, and optimized for performance. No guesswork — just a proven system ready for deployment",
+    icon: WeProvideStakingIcon
+  },
+  {
+    title: "Blockchain Integration",
+    text: "We handle the heavy lifting so you don’t have to. Whether you need Ethereum, Solana, Binance Smart Chain, or a custom network, our solution  is pre-configured for easy deployment across multiple protocols",
+    icon: BlockChainIcon
+  },
+  {
+    title: "Bank-Grade Security  from Day One",
+    text: "Security isn’t an afterthought — it’s built into the core of our secure staking software. Our infrastructure includessmart contract audits to prevent vulnerabilities, multi-layer security protocols for asset protection, automated monitoring to detect and prevent fraudulent activity",
+    icon: BankGradeSecurIcon
+  },
+  {
+    title: "You Customize to Match Your Brand & Strategy",
+    text: "Unlike rigid, off-the-shelf solutions, our platform adapts to your needs. You get full branding control — apply your logo, colors, and domain; flexible staking mechanics, set lock-up periods, APYs, and custom rewards, multi-asset support, Integrate your token or allow users to stake multiple assets",
+    icon: YouCustomizeIcon
+  },
+  {
+    title: "Fast Deployment & Ongoing Support",
+    text: "Why spend months building when you can go live in weeks? With our white label staking platform, you geta rapid setup process — start onboarding users fast, technical support — our team ensures a smooth launch and ongoing updates and scalability from day one — as your user base grows, your staking platform scales effortlessly",
+    icon: FastDevIcon
+  }
+];
