89c8188216efc4f229988c64fc67db5117a8edbe
Viktor Shaparenko <vkshadyafk@gmail.com>
2025-06-06 16:39:01 +0300

    fix/cases-optimization(memelut optimization)

--- src/app/(dashboard)/cases/memelut/components/MemelutBannerContainer/index.jsx
+++ src/app/(dashboard)/cases/memelut/components/MemelutBannerContainer/index.jsx
@@ -2,12 +2,12 @@
 import { useMediaQuery } from "@chakra-ui/react";
 import React from "react";
 import Banner from "../../../components/Banner";
+import bannerImg from "@/assets/cases/memelut/memelut-banner.webp";
+import bannerImgMobile from "@/assets/cases/memelut/memolut-banner-mob.png";
 
 const MemelutBannerContainer = () => {
   const [isMoreThan768] = useMediaQuery("(min-width: 768px)");
-  const currentBanner = isMoreThan768
-    ? "/cases/memelut/memelut-banner.webp"
-    : "/cases/memelut/memolut-banner-mob.png";
+  const currentBanner = isMoreThan768 ? bannerImg : bannerImgMobile;
 
   return (
     <Banner
--- src/app/(dashboard)/cases/memelut/page.jsx
+++ src/app/(dashboard)/cases/memelut/page.jsx
@@ -23,6 +23,8 @@ export { metadata };
 const MemelutHeaderContainer = dynamic(
   () => import("./components/MemelutHeaderContainer")
 );
+const MemelutContent = dynamic(() => import("./components/MemelutContent"));
+
 const Memelut = () => {
   const breadcrumbList = setBreadcrumbSchema(
     "Cases",
@@ -38,47 +40,7 @@ const Memelut = () => {
       />
       <section>
         <MemelutHeaderContainer />
-        <MemelutOverview containerClasses="xl:pb-[120px]" />
-        <CasesSlider
-          sectionClasses={"h-[226px] md:mb-[120px] py-5 md:py-0 md:h-[443px]"}
-          images={memelutSlides}
-          itemClasses={"pl-0 ml-[13px]  md:ml-7"}
-        />
-
-        <div className="mx-auto w-full max-w-[1246px] px-4 md:px-10 xl:px-0">
-          {" "}
-          <MemelutSolutions
-            solutions={memelutSolutions}
-            classes="md:gap-[85px] xl:last:-mt-0"
-            containerClasses="w-full"
-            gridClass="xl:gap-[50px]"
-          />
-        </div>
-
-        <MemelutWhatWeDid />
-
-        <Image
-          width={1349}
-          height={269}
-          src="/cases/memelut/memelut-coins-banner.webp"
-          className="mx-auto h-auto w-[calc(100vw-6px)] max-w-[1349px]"
-          loading="lazy"
-          priority={false}
-        />
-
-        <TeamBehind
-          headerClasses={"xl:leading-[18.41px] xl:tracking-[1.84px]"}
-          team={memelutTeam}
-          classes={"pt-0 xl:pt-[60px]"}
-        />
-
-        <MoreProjects projects={relatedMemelutProjects} isReversed={true} />
-
-        <Contact
-          className={"px-5 py-[50px] md:px-0 xl:py-[143px]"}
-          descriptionClasses={"hidden md:block"}
-          titleClasses={"mb-8 md:mb-0"}
-        />
+        <MemelutContent />
       </section>
     </>
   );
