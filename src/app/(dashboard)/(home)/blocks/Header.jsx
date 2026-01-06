42bec8290ef9253fac2535afbe657b1d3b8c0298
Viktor Shaparenko <vkshadyafk@gmail.com>
2026-01-06 23:08:02 +0200

    feat/hero-section-update

--- src/app/(dashboard)/(home)/blocks/Header.jsx
+++ src/app/(dashboard)/(home)/blocks/Header.jsx
@@ -5,12 +5,12 @@ const HeaderContent = dynamic(() => import("./HeaderContent"));
 const Header = () => {
   return (
     <section
-      className=" -mt-[230px] overflow-hidden border-b border-b-th-fade md:-mt-0 md:h-[861px]"
+      className=" -mt-[230px] overflow-hidden border-b border-b-th-fade md:-mt-0 lg:h-[861px] xl:h-[calc(100vh-4rem)] xl:min-h-[861px]"
       id="header"
     >
-      <div className="video-wrapper min-h-full bg-transparent  md:h-[861px]">
+      <div className="video-wrapper min-h-full bg-transparent  ">
 
-        <div className="relative flex min-h-full w-full flex-col md:h-[861px]">
+        <div className="relative flex min-h-full w-full flex-col ">
           <HeaderContent />
         </div>
       </div>
--- src/app/(dashboard)/(home)/blocks/ProjectsLine.jsx
+++ src/app/(dashboard)/(home)/blocks/ProjectsLine.jsx
@@ -7,11 +7,11 @@ import { cn } from "@/lib/utils";
 
 export default function ProjectsLine() {
   return (
-    <div className="w-full py-[51px] border-t-[0.5px] border-[#FFFFFF80]">
+    <div className="w-full py-[51px] max-md:py-[39px] border-t-[0.5px] border-[#FFFFFF80]">
       <Marquee direction="left" speed={50} pauseOnHover={true}>
-        <div className="flex items-center gap-30">
+        <div className="flex items-center gap-30 max-lg:gap-10">
           {projectLogos.map((logo, index) => (
-            <Link key={logo.name} href={logo.linkUrl} target="_blank" rel="noopener noreferrer" className={cn(projectLogos.length === index + 1 && "mr-30")}>
+            <Link key={logo.name} href={logo.linkUrl} target="_blank" rel="noopener noreferrer" className={cn("max-md:scale-75", projectLogos.length === index + 1 && "mr-30 max-lg:mr-15")}>
               <logo.logo />
             </Link>
           ))}
